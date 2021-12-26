38fd9b3df0b17128c88dff0d64122bec70954e83
gaminem9 <gaminem9@gmail.com>
2021-12-26 10:25:39 +0900

    [COM] ProtocolTag 추가

---
+++ CustomFileTransport/CFT_Client/ProtocolTag.h
@@ -0,0 +1,9 @@
+#pragma once
+
+enum class ProtocolTag
+{
+	CHAT_NORMAL = 101,
+	CHAT_WHISPER = 102,
+	ROOM_USER_LIST = 501,
+	TAG_EXIT = 999
+};
---
+++ CustomFileTransport/CFT_Server/ClientInfo.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+
+typedef struct ClientInfo
+{
+	int Socket;
+	std::string NickName;
+};
