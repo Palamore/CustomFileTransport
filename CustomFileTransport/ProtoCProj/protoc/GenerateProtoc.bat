fa5b2b5ffaf2af297456f6d341bfc6acb303b7c6
gaminem9 <gaminem9@gmail.com>
2021-12-31 16:24:54 +0900

    [protoc] 배치파일 수정

--- CustomFileTransport/ProtoCProj/protoc/GenerateProtoc.bat
+++ CustomFileTransport/ProtoCProj/protoc/GenerateProtoc.bat
@@ -2,9 +2,11 @@
 
 cd C:\CustomFileTransport\CustomFileTransport\ProtoCProj\protoc
 
-protoc --cpp_out=C:\CustomFileTransport\CustomFileTransport\CFT_Client ClientInfo.proto
-protoc --cpp_out=C:\CustomFileTransport\CustomFileTransport\CFT_Client PacketTag.proto
-protoc --cpp_out=C:\CustomFileTransport\CustomFileTransport\CFT_Server ClientInfo.proto
-protoc --cpp_out=C:\CustomFileTransport\CustomFileTransport\CFT_Server PacketTag.proto
+set ClientPath=C:\CustomFileTransport\CustomFileTransport\CFT_Client
+set ServerPath=C:\CustomFileTransport\CustomFileTransport\CFT_Server
+protoc --cpp_out=%ClientPath% ClientInfo.proto
+protoc --cpp_out=%ClientPath% PacketTag.proto
+protoc --cpp_out=%ServerPath% ClientInfo.proto
+protoc --cpp_out=%ServerPath% PacketTag.proto
 
 pause
